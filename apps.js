6500bfb550b8318c4a9304edf294b0a71ce3e8d6
MuriloPetruci <murilopetruci@hotmail.com>
2019-08-19 06:34:49 -0300

    WebSocket

---
+++ apps.js
@@ -0,0 +1,14 @@
+//ANTIGO N É ATUAL
+// WebSockets nos permitem fazer uma comunicação bi direcional entre clientes e servidores. quando fazemos  uma REQ ao servidor e o cliente que esta pedindo uma informação e o servidor apenas respondendo então essa conexão é fechada, com o websockets a conexão permenesse aberta e nessa conexão pode ser enviados varios dados de uma vez só e o clinete pode mandar varoios dados pro servidor e o servidor varios dados para o cliente sem ele ter requisitado nada, com isso a velocidade da informação aumenta. 
+// trabalhando na porta 3 mil
+var io = require('socket.io')(3000);
+
+//listen chamado connection quando o cliente se conectar ele vai executar essa função e ela recebe um socket
+
+io.on('connection', (socket)=>{
+    console.log('novo usuario conectado');
+
+    socket.on('client_hello', (data)=>{
+        io.sockets.emit('server_hello', data);
+    });
+});
